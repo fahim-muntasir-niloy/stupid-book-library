89a4179819012c8770891f0fd87a7ff66de082ad
niloy1601107 <niloyf3@gmail.com>
2021-08-19 01:49:54 +0600

    started adding js and finalizedd html and css

---
+++ event.js
@@ -0,0 +1,12 @@
+// declaring variables for ids
+let book = document.getElementById("Book");
+let author = document.getElementById("Author");
+let date = document.getElementById("Date")
+
+let submitBtn = document.getElementById("submitBtn")
+
+submitBtn.addEventListener("click",entry)
+
+function entry(){
+    alert('working')
+}
